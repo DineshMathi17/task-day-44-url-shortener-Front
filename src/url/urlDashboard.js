b40f4c32c956fd560db64cc4890005fd62a84398
DineshMathi17 <121874583+DineshMathi17@users.noreply.github.com>
2023-07-09 18:46:59 +0530

    Update urlDashboard.js

--- src/url/urlDashboard.js
+++ src/url/urlDashboard.js
@@ -107,7 +107,7 @@ export function UrlShortener() {
               </form>
             </div>
           </MDBCardBody>
-          {newdata ? <p style={{ backgroundColor: "green", color: "white" }}>LINK: http://localhost:8000/url/{newdata.data.shortId}</p> : ""}
+          {newdata ? <p style={{ backgroundColor: "green", color: "white" }}>LINK: https://task-day-44-url-backend.onrender.com/url/{newdata.data.shortId}</p> : ""}
           <div className='date-month'>
             <p>Number of Link created a Day: <span>{daycount}</span></p>
             <p>Number of Link created a Month: <span>{monthcount}</span></p>
